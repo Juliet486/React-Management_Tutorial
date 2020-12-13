7ea18e91cfc8d37b02f4d6fdc3d5e7edaffd07f3
juliet486 <juliet486@hanmail.net>
2020-12-14 07:58:41 +0900

    Create React Project

---
+++ management/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <img src={logo} alt="logo"/>
+      <h2>Let's develop management system! with me</h2>
+      
+    </div>
+  );
+}
+
+export default App;
